773b59163a97a0243ee5bd3f807ac54edc2a02ca
lucasviana125 <129747155+lucasviana125@users.noreply.github.com>
2024-07-01 08:53:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaresultados = document.querySelector(".caixa-resultados");
+const textoResultados = document.querySelector(".texto-resultados");
+
+const perguntas = [
+    {
+    enunciado: "pergunta 1",
+    alternativas: ["alternativa 1",
+        "alternativa 2"]
+    },
+
+{enunciado: "pergunta 2"
+    alternativas: ["alternativa 1",
+    "alternativa 2"]
+},
+]
